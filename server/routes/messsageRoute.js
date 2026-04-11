e6c29865f71606d75d66b7b8011daf0c8c088fbb
rajawatshivpratap46 <shiva18021912@gmail.com>
2026-04-11 16:42:07 +0530

    Backend is completede and now we are moving to other stage

---
+++ server/routes/messsageRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.js";
+import { getMessages, getUsersSidebar, markMessageAsSeen, sendMessage } from "../controllers/messagecontroller";
+
+const messageRouter = express.Router();
+
+messageRouter.get("/users",protectRoute,getUsersSidebar);
+messageRouter.get("/:id",protectRoute,getMessages);
+messageRouter.put("mark/:id",protectRoute,markMessageAsSeen);
+messageRouter.post("/send:id",protectRoute,sendMessage)
+
+export default messageRouter;
